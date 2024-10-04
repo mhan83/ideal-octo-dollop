a54bd4806fd0ddc7afffbdabc4c3801813340bd7
Mike Han <mike.han@saucelabs.com>
2024-10-04 11:24:57 -0600

    test in a more npm like fashion

--- index.mjs
+++ index.mjs
@@ -1,70 +1,106 @@
-import fs from 'node:fs';
-import { basename } from 'node:path';
-import os from 'node:os';
+import { createWriteStream } from 'node:fs';
+import { mkdir } from 'node:fs/promises';
+import { basename, join } from 'node:path';
 import path from 'node:path';
 import { Readable } from 'node:stream';
 import { finished } from 'node:stream/promises';
-import { promisify } from 'node:util';
-import child_process from 'node:child_process';
+import * as tar from 'tar';
 
-const exec = promisify(child_process.exec);
+import { packages } from './packages.mjs';
 
-/**
- * @param {string} url 
- * @param {string} dest 
- */
 async function download(url, dest = './') {
   const destFile = path.join(dest, basename(url));
 
-  console.time('download');
-
   const res = await fetch(url);
-  const fileStream = fs.createWriteStream(destFile);
+  const fileStream = createWriteStream(destFile);
   await finished(Readable.fromWeb(res.body).pipe(fileStream));
-
-  console.timeEnd('download');
 
   return destFile;
 }
 
-/**
- * @param {string} source 
- * @param {string} dest 
- */
-async function unzip(source, dest = './') {
-  console.time('unzip');
-
-  // await exec(`unzip -q -o ${source}`, {
-  //   cwd: path.dirname(dest),
-  // });
-  await exec(`python extract.py ${source} ./`, {
-    cwd: path.dirname(dest),
+async function prep(dir) {
+  await mkdir(dir, {
+    recursive: true,
   });
-
-  console.timeEnd('unzip');
 }
 
-async function unzipWin(source, dest = './') {
-  console.time('unzip');
+const workers = parseInt(process.argv[2]) ?? 5;
+const root = process.argv[3] ?? './';
+const dest = join(root, 'packages');
 
-  await exec(`Expand-Archive -Force -Path ${source}`, {
-    cwd: path.dirname(dest),
-    shell: 'powershell.exe',
-  });
+console.log(`testing with ${workers} workers downloading to ${dest}`);
 
-  console.timeEnd('unzip');
-}
+await prep(dest);
 
-const urlArg = process.argv[2];
-const destArg = process.argv[3] ?? './';
+let iter = packages.values();
 
-console.log(`downloading ${urlArg} to ${destArg}`);
-console.log();
+const downloadWorkers = Array(workers).fill(iter).map(async (iter) => {
+  for (const p of iter) {
+    console.time(`downloading ${basename(p)}`);
+    await download(p, dest);
+    console.timeEnd(`downloading ${basename(p)}`);
+  }
+});
 
-const destFile = await download(urlArg, destArg);
-await unzip(destFile);
-// if (os.platform().startsWith('win')) {
-//   await unzipWin(destFile);
-// } else {
-//   await unzip(destFile);
+console.time(`download ${packages.length} packages`);
+await Promise.allSettled(downloadWorkers);
+console.timeEnd(`download ${packages.length} packages`);
+
+iter = packages.values();
+const extractWorkers = Array(workers).fill(iter).map(async (iter) => {
+  for (const p of iter) {
+    const name = basename(p, '.tgz')
+    await mkdir(join(dest, name), { recursive: true });
+    await tar.x({
+      cwd: join(dest, name),
+      f: join(dest, basename(p)),
+      newer: false,
+    });
+  }
+});
+
+console.time(`extract ${packages.length} packages`);
+await Promise.allSettled(extractWorkers);
+console.timeEnd(`extract ${packages.length} packages`);
+// 
+// /**
+//  * @param {string} source 
+//  * @param {string} dest 
+//  */
+// async function unzip(source, dest = './') {
+//   console.time('unzip');
+// 
+//   // await exec(`unzip -q -o ${source}`, {
+//   //   cwd: path.dirname(dest),
+//   // });
+//   await exec(`python extract.py ${source} ./`, {
+//     cwd: path.dirname(dest),
+//   });
+// 
+//   console.timeEnd('unzip');
 // }
+// 
+// async function unzipWin(source, dest = './') {
+//   console.time('unzip');
+// 
+//   await exec(`Expand-Archive -Force -Path ${source}`, {
+//     cwd: path.dirname(dest),
+//     shell: 'powershell.exe',
+//   });
+// 
+//   console.timeEnd('unzip');
+// }
+// 
+// const urlArg = process.argv[2];
+// const destArg = process.argv[3] ?? './';
+// 
+// console.log(`downloading ${urlArg} to ${destArg}`);
+// console.log();
+// 
+// await download();
+// await unzip(destFile);
+// // if (os.platform().startsWith('win')) {
+// //   await unzipWin(destFile);
+// // } else {
+// //   await unzip(destFile);
+// // }
